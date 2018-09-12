525b5bd57ffc2cb0008b54af82fe158f2e7114e5
John-Anthony <41270598+JaBautista93@users.noreply.github.com>
2018-09-12 00:19:43 -0500

    updated file structure, updated html, started css, and started JS file

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// Create Variables to hold references
+var RandNum = document.getElementById('RandNum');
+var Status = document.getElementById('Status');
+var Win = document.getElementById('Win');
+var Loss = document.getElementById('Loss');
+var button = document.getElementById('btnClose');
+var TotalScore = document.getElementById('TotalScore');
+var TotalCounter = document.getElementById('TotalCounter')
+
+
+
+// Used this link to make my buttons
+<script async src="//jsfiddle.net/jquerybyexample/2QbgP/embed/"></script>
+
