c5eb0eae1ff990fc67d8584e2c2fadba8fd2b311
John-Anthony <41270598+JaBautista93@users.noreply.github.com>
2018-09-13 20:26:33 -0500

    updated gummy buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,39 @@
+
 // Create Variables to hold references
-var RandNum = document.getElementById('RandNum');
-var Status = document.getElementById('Status');
-var Win = document.getElementById('Win');
-var Loss = document.getElementById('Loss');
-var button = document.getElementById('btnClose');
-var TotalScore = document.getElementById('TotalScore');
-var TotalCounter = document.getElementById('TotalCounter')
+$("#randNum").text(RandNum)
+//$("#Status").text(Status)
+$("#Win").text(Win)
+$("#Loss").text(Loss)
+//$("#button").text(button)
+$("#TotalScore").text(TotalScore)
+$("#TotalCounter").text(TotalCounter)
+
+//This section holds variables
+
+//Random number generator for the computer
+var RandNum = Math.floor((Math.random() * 102) + 19); 
+    console.log(RandNum)
+    $("#randNum").text(RandNum)
+
+//This tells us how many wins and losses
+var win = 0
+var loss = 0
+
+//create a random number for the buttons
+    $(".button").on("click", function() {
+        var RandGumNum = Math.floor((Math.random() * 12) + 1)
+        console.log(RandGumNum)
+    });
+//cli
 
 
 
-// Used this link to make my buttons
-<script async src="//jsfiddle.net/jquerybyexample/2QbgP/embed/"></script>
 
-//Random number generator
-var RandNum = 19-120
 
-//wins and losses
-var win = o;
-var loss = 0;
+ //wins and losses
 
-//button
-var button = 1-12;
+
+
+
+
 
